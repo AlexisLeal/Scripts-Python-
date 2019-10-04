b7cc4c4e55828e6a8f624f43113886fc2bb01630
AlexisLeal <31640145+AlexisLeal@users.noreply.github.com>
2019-10-04 00:23:12 -0500

    Create abecedario.py

---
+++ abecedario.py
@@ -0,0 +1,16 @@
+#Abecedario 
+
+
+AbecedarioMin = map(chr,range(97,123))#Minisculas
+AbecedarioMay = map(chr,range(65,91)) #mayusculas 
+
+for i in AbecedarioMay:
+  print(i,end="")
+
+print("\n")
+
+for i in AbecedarioMin:
+  print(i,end="")
+
+
+print("\n")
